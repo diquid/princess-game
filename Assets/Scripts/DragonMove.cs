0cbb9f4df844bedf7934df35f6ba111f712d7816
DILYARA <fatykhova.dilyara@mail.ru>
2021-05-12 15:11:46 +0500

    health

--- Assets/Scripts/DragonMove.cs
+++ Assets/Scripts/DragonMove.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 public class DragonMove : MonoBehaviour
 {
     public List<Transform> points;
-    public int nextID;
+    public int nextID = 0;
     private int idChangeValue = 1;
+    public float speed = 2;
 
 
     private void Reset()
@@ -34,6 +35,29 @@ public class DragonMove : MonoBehaviour
         points = new List<Transform>();
         points.Add(p1.transform);
         points.Add(p2.transform);
+    }
+
+    private void Update()
+    {
+        MoveToNextPoint();
+    }
+
+    void MoveToNextPoint()
+    {
+        Transform goalPoint = points[nextID];
+        if (goalPoint.transform.position.x > transform.position.x)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else
+            transform.localScale = new Vector3(1, 1, 1);
+        transform.position = Vector2.MoveTowards(transform.position, goalPoint.position, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, goalPoint.position) < 0.2f)
+        {
+            if (nextID == points.Count - 1)
+                idChangeValue = -1;
+            if (nextID == 0)
+                idChangeValue = 1;
+            nextID += idChangeValue;
+        }
     }
 
 
